8c615c37e7e1feead183d6428e826a536dd0d084
khoahoang555 <khoahoangyahoo@gmail.com>
2023-02-26 21:54:43 +0700

    First commit: Create project

---
+++ spring-ecommerce/src/main/java/com/ecommerce/springecommerce/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.ecommerce.springecommerce.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<>("Application Running!", HttpStatus.OK);
+    }
+}
